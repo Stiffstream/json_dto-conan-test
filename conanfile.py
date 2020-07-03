9bd91c3a9a12e47ced115c6d33bbb5a67406209f
Chris Mc <prince.chrismc@gmail.com>
2020-07-03 15:09:27 -0400

    Update conanfile.py
    plus bumping version

--- conanfile.py
+++ conanfile.py
@@ -2,40 +2,47 @@ from conans import ConanFile, CMake, tools
 import os
 
 class JsondtoConan(ConanFile):
-    name = "json-dto"
-    version = "0.2.9.1"
+    name = "json_dto"
+    version = "0.2.9.2"
     license = "BSD-3-Clause"
     author = "Stiffstream info@stiffstream.com"
     url = "https://github.com/Stiffstream/json_dto-conan"
     description = "A small header-only helper for converting data between json representation and c++ structs"
     topics = ("JSON", "DTO", "Serialization")
-    settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    source_subfolder = "json_dto"
 
-    requires = "rapidjson/1.1.0@bincrafters/stable"
+    _cmake = None
+    
+    @property
+    def _source_subfolder(self):
+        return "source_subfolder"
+
+    def requirements(self):
+        self.requires("rapidjson/1.1.0")
 
     def source(self):
         # https://github.com/Stiffstream/json_dto/archive/v.0.2.8.tar.gz
         source_url = "https://github.com/Stiffstream/json_dto/archive"
         tools.get("{0}/v.{1}.tar.gz".format(source_url, self.version))
-        extracted_dir = "json_dto-v." + self.version
-        os.rename(extracted_dir, self.source_subfolder)
+        extracted_dir = self.name + "-v." + self.version
+        os.rename(extracted_dir, self._source_subfolder)
 
-
-    def build(self):
-        cmake = CMake(self)
+    def _configure_cmake(self):
+        if self._cmake:
+            return self._cmake
+        self._cmake = CMake(self)
         cmake.definitions['JSON_DTO_INSTALL'] = True
         cmake.definitions['JSON_DTO_FIND_DEPS'] = False
-        cmake.configure(source_folder = self.source_subfolder + "/dev/json_dto")
+        cmake.configure(source_folder = self._source_subfolder + "/dev/json_dto")
+        
+    def build(self):
+        cmake = self._configure_cmake()
         cmake.build()
-        cmake.install()
 
     def package(self):
-        self.copy("*.hpp", dst="include/json_dto", src=self.source_subfolder + "/dev/json_dto")
+        cmake = self._configure_cmake()
+        cmake.install()
         self.copy("license*", src=self.source_subfolder, dst="licenses", ignore_case=True, keep_path=False)
 
     def package_id(self):
         self.info.header_only()
-
-
